8c0ab95b9a393ce3ff935a1124363e8e9037b693
Conor Mendenhall <conor.mendenhall@gmail.com>
2019-12-17 22:00:48 -0500

    querying images from contentful

--- src/components/media.js
+++ src/components/media.js
@@ -2,48 +2,38 @@ import React from 'react'
 import {useStaticQuery, graphql} from 'gatsby'
 
 const Media = () => {
-    return <p>Hello media</p>
-//   const data = useStaticQuery(graphql`
-//   query {
-//     allContentfulMenuItem(
-//       filter: {
-//         category: {
-//           title: {eq: "Breakfast"}
-//         }
-//       }
-//     ) {
-//       edges {
-//         node {
-//           title
-//           id
-//           price
-//           category{
-//             title
-//           }
-//         }
-//       }
-//     }
-//   }
-//   `)
+  const data = useStaticQuery(graphql`
+  query {
+    allContentfulImage(
+      filter: {
+      }
+    ) {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+  `)
 
-//   const items = data.allContentfulMenuItem.edges;
-//   return (
-//     <div>
-//       <p className="sample">SAMPLE MENU</p>
-//       <ul className="menu-items-grid">
-//         {
-//           items.map(({ node }) => {
-//             return (
-//               <li key={node.id} className="menu-item">
-//                 <h3>{node.title}</h3>
-//                 <span>£{node.price}</span>
-//               </li>
-//             )
-//           })
-//         }
-//       </ul>
-//     </div>
-//   )
+  const items = data.allContentfulImage.edges;
+  return (
+    <div>
+      <p className="sample">SAMPLE MEDIA</p>
+      <ul className="menu-items-grid">
+        {
+          items.map(({ node }) => {
+            return (
+              <li className="menu-item">
+                <h3>{node.title}</h3>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
 }
 
 export default Media;
