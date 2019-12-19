02e8d6a4a3400b3703792660a5a12b18c579a0e3
Conor Mendenhall <conor.mendenhall@gmail.com>
2019-12-19 16:21:22 -0500

    changes to index content

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,15 @@ import Img from 'gatsby-image'
 import Instagram from '../components/instagram'
 import LeafletMap from '../components/leafletMap'
 import Footer from '../components/footer'
-import Media from '../components/media'
 
 const IndexPage = ({data}) => {
 
 return (
   <Layout>
     <SEO title="Home" />
-    {/* <section className="hero">
-      <Img fluid={data.coffeeShop.childImageSharp.fluid} className="hero-image"/>
-    </section> */}
+    <section className="hero">
+      <Img fluid={data.collage.childImageSharp.fluid} className="hero-image"/>
+    </section>
     <section className="container">
       <span className="description">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -26,23 +25,18 @@ return (
         <Img fluid={data.freewayMap.childImageSharp.fluid} className="gallery-img2"/>
         <Img fluid={data.hastings.childImageSharp.fluid} className="gallery-img3"/>
       </div>
-      <div className="about-grid">
-        <h2>WE'RE CRAZY ABOUT COFFEE</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
     </section>
     <div className="parallax"></div>
-    <section className="container">
-      <Media />
-      <Instagram />
-    </section>
     {typeof window !== 'undefined' &&
       <LeafletMap
-        position={[55.952103, -3.196175]} // Your Coordinates
-        zoom={18} // Zoom Level
-        markerText={"Local Cafe, 65 Park Row"} // Icon text
+        position={[42.3408, -83.0370]} // Your Coordinates
+        zoom={15} // Zoom Level
+        markerText={"Black Bottom"} // Icon text
       />
     }
+    <section className="container">
+      <Instagram />
+    </section>
     <Footer />
   </Layout>
   )
@@ -60,7 +54,7 @@ export const fluidImage = graphql`
 
 export const pageQuery = graphql`
   query {
-    coffeeShop: file(relativePath: { eq: "coffee-shop-exterior.jpg" }) {
+    collage: file(relativePath: { eq: "collage.jpg" }) {
       ...fluidImage
     }
     freewayMap: file(relativePath: { eq: "freeway-map.jpg" }) {
