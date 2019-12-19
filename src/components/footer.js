5c244ebf9fc563e310c61d5871deafe54aef64b7
Conor Mendenhall <conor.mendenhall@gmail.com>
2019-12-19 16:23:37 -0500

    clean up footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,20 +11,7 @@ const Footer = () => {
           <a href="https://www.facebook.com/BlackBottomArchives/">facebook</a>
           <a href="http://www.instagram.com/blackbottomarchives">instagram</a>
         </div>
-        <div className="hours">
-          <h3>HOURS</h3>
-          <span>Weekdays / 8-5</span>
-          <span>Weekdays / 10-5</span>
-        </div>
-        <div className="contact">
-          <h3>CONTACT</h3>
-          <span>01632 960966</span>
-          <span>@yourSocialName</span>
-        </div>
       </section>
-      <p>Built by
-        <a href="https://crollacreations.com" target="_blank" rel="noopener noreferrer">crollacreations</a>
-      </p>
     </footer>
   )
 }
