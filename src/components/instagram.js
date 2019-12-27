f35e59dddf6b7602f1f90c8765a8fa021b8b2a02
Conor Mendenhall <conor.mendenhall@gmail.com>
2019-12-27 12:13:14 -0500

    remove unneeded insta queries

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -1,63 +1,63 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image';
-
+import Img from 'gatsby-image'
 
 const Instagram = () => {
   const data = useStaticQuery(graphql`
-  query {
-    allInstaNode(
-      limit:6,
-      sort: {
-        fields: [timestamp]
-        order: [DESC]
-      }
-    ) {
-      edges {
-        node {
-          id
-          likes
-          comments
-          mediaType
-          preview
-          original
-          timestamp
-          localFile {
-            childImageSharp {
-              fixed(width: 280, height: 280) {
-                ...GatsbyImageSharpFixed
+    query {
+      allInstaNode(limit: 6, sort: { fields: [timestamp], order: [DESC] }) {
+        edges {
+          node {
+            id
+            likes
+            localFile {
+              childImageSharp {
+                fixed(width: 280, height: 280) {
+                  ...GatsbyImageSharpFixed
+                }
               }
             }
           }
         }
       }
     }
-  }
   `)
-  const instaPosts = data.allInstaNode.edges;
+  const instaPosts = data.allInstaNode.edges
   return (
     <div className="insta">
-      <a href="https://www.instagram.com/blackbottomarchives/" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.instagram.com/blackbottomarchives/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <h2>INSTAGRAM</h2>
         <span>@blackbottomarchives</span>
       </a>
       <ul className="insta-posts">
-          {instaPosts.map(({ node }) => {
-            return (
-              <a href={"https://www.instagram.com/p/" + node.id + "/"} target="_blank" rel="noopener noreferrer">
-                <li key={node.id} className="insta-post">
-                  <Img fixed={node.localFile.childImageSharp.fixed} />
-                  <div className="overlay">
-                    <img className="icon" src={require('../images/like.png')} alt="like icon"/>
-                    <p className="text">{node.likes}</p>
-                  </div>
-                </li>
+        {instaPosts.map(({ node }) => {
+          return (
+            <li key={node.id} className="insta-post">
+              <a
+                href={'https://www.instagram.com/p/' + node.id + '/'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Img fixed={node.localFile.childImageSharp.fixed} />
+                <div className="overlay">
+                  <img
+                    className="icon"
+                    src={require('../images/like.png')}
+                    alt="like icon"
+                  />
+                  <p className="text">{node.likes}</p>
+                </div>
               </a>
-            )
-          })}
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
 }
 
-export default Instagram;
+export default Instagram
