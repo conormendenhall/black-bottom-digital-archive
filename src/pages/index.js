461a1423878155280267ef7572d3e3928aacc733
Conor Mendenhall <conor.mendenhall@gmail.com>
2019-12-28 15:44:45 -0500

    testing build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,51 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Instagram from '../components/instagram'
 import LeafletMap from '../components/leafletMap'
 import Footer from '../components/footer'
 
-const IndexPage = ({data}) => {
-
+const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
       <section className="hero">
-        <Img fluid={data.collage.childImageSharp.fluid} className="hero-image"/>
+        <Img
+          fluid={data.collage.childImageSharp.fluid}
+          className="hero-image"
+        />
       </section>
       <section className="container">
         <span className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </span>
         <div className="gallery-grid">
-          <Img fluid={data.ebonyRoom.childImageSharp.fluid} className="gallery-img1"/>
-          <Img fluid={data.freewayMap.childImageSharp.fluid} className="gallery-img2"/>
-          <Img fluid={data.hastings.childImageSharp.fluid} className="gallery-img3"/>
+          <Img
+            fluid={data.ebonyRoom.childImageSharp.fluid}
+            className="gallery-img1"
+          />
+          <Img
+            fluid={data.freewayMap.childImageSharp.fluid}
+            className="gallery-img2"
+          />
+          <Img
+            fluid={data.hastings.childImageSharp.fluid}
+            className="gallery-img3"
+          />
         </div>
       </section>
       <div className="parallax"></div>
-      {typeof window !== 'undefined' &&
+      {typeof window !== 'undefined' && (
         <LeafletMap
-          position={[42.3408, -83.0370]} // Your Coordinates
+          position={[42.3408, -83.037]} // Your Coordinates
           zoom={15} // Zoom Level
           places={data.places.edges}
         />
-      }
+      )}
       <section className="container">
         <Instagram />
       </section>
@@ -50,7 +62,7 @@ export const fluidImage = graphql`
       }
     }
   }
-`;
+`
 
 export const pageQuery = graphql`
   query {
@@ -89,6 +101,6 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
 
 export default IndexPage
