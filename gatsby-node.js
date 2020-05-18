ae3c6fa5accea7ccc778b8f577016729c45e37c4
Conor Mendenhall <conor.mendenhall@gmail.com>
2020-05-17 22:23:43 -0400

    pages for each historical site

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,54 @@
  */
 
 // You can delete this file if you're not using it
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const path = require('path')
+
+  // Query for entries to use in creating pages
+  const result = await graphql(
+    `
+      query {
+        sites: allContentfulHistoricalSite {
+          edges {
+            node {
+              id
+              title
+              slug
+              abstract {
+                id
+                abstract
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  // Create pages for each historical site
+  const historicalSiteComponent = path.resolve(
+    `src/components/historical-site.js`
+  )
+  result.data.sites.edges.forEach(({ node }) => {
+    const path = node.slug
+    createPage({
+      path,
+      component: historicalSiteComponent,
+      // In your blog post template's graphql query, you can use pagePath
+      // as a GraphQL variable to query for data from the markdown file.
+      context: {
+        pagePath: path,
+        title: node.title,
+      },
+    })
+  })
+}
---
+++ src/components/historical-site.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HistoricalSite = ({ pageContext }) => {
+  return (
+    <div className="historical-site">
+      <h1>{pageContext.title}</h1>
+    </div>
+  )
+}
+
+export default HistoricalSite
