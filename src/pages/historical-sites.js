dd357eea544d44f6b62811a08b328cfea2cf1590
Conor Mendenhall <conor.mendenhall@gmail.com>
2020-07-15 22:38:21 -0400

    historical site card gallery

--- src/pages/historical-sites.js
+++ src/pages/historical-sites.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const HistoricalSitesPage = ({ data }) => {
   let sites = data.sites.edges.map((item, key) => (
-    <div key={item.node.id} className="entry-link">
-      <a href={`/historical-sites/${item.node.slug}`}>
-        <span>{item.node.title}</span>
-      </a>
-    </div>
+    <a key={item.node.id} href={`/historical-sites/${item.node.slug}`}>
+      <div className="card">
+        {item.node.image && <Img fluid={item.node.image.fluid} />}
+        <div className="card-title">
+          <span>{item.node.title}</span>
+        </div>
+      </div>
+    </a>
   ))
 
   return (
@@ -19,7 +23,9 @@ const HistoricalSitesPage = ({ data }) => {
       <SEO title="Historical Sites" />
       <section className="container">
         <h1>Historical Sites</h1>
-        {sites}
+        <div id="historicalSites" className="card-gallery">
+          {sites}
+        </div>
       </section>
     </Layout>
   )
