1980197de4f149c72014c5e95af210e704f119e6
Conor Mendenhall <conor.mendenhall@gmail.com>
2020-07-15 22:38:32 -0400

    contact call to action

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,44 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact Us</h1>
-      <p>Contact Us Content Here</p>
+      <p>
+        Did you or your family members live, work, or play in Black Bottom? Do
+        you have stories, photos or other media to share? Please contact us for
+        more information about how you can contribute to this growing digital
+        archive.
+      </p>
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSe4fk6G3nPZ6UkkK9IfHI9J3a5HQBeqg6oKmWrR8uNgfNyk1w/viewform"
+        target="_blank"
+      >
+        <div className="call-to-action">
+          <span>Share Your History</span>
+        </div>
+      </a>
+      <p>
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSe4fk6G3nPZ6UkkK9IfHI9J3a5HQBeqg6oKmWrR8uNgfNyk1w/viewform"
+          target="_blank"
+        >
+          Use this form to submit your oral histories or photos.
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://calendly.com/pgwatkins/oralhistoryinterviews?month=2020-07"
+          target="_blank"
+        >
+          Use this link to schedule an oral history interview with our Archival
+          team!
+        </a>
+      </p>
+      <p>
+        Contact us directly if you have any other media or artifacts that you
+        would like to share
+        <a href="mailto:hello@blackbottomarchives.com?subject=BBA Content Submission">
+          <p>hello@blackbottomarchives.com</p>
+        </a>
+      </p>
     </div>
   )
 }
