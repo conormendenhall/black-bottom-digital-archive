4454e8ac453ba5cd68495327c33996c170698c96
Conor Mendenhall <conor.mendenhall@gmail.com>
2020-08-02 16:37:30 -0400

    footer icons + styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,42 @@
 import React from 'react'
 
-const Footer = () => {
+import {
+  AiOutlineTwitter,
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+} from 'react-icons/ai'
 
+const Footer = () => {
   return (
     <footer>
       <section>
+        <h3>Connect</h3>
         <div className="social">
-          <h3>Connect</h3>
-          <a href="http://twitter.com/_blackbottom">Twitter</a>
-          <a href="https://www.facebook.com/BlackBottomArchives/">Facebook</a>
-          <a href="http://www.instagram.com/blackbottomarchives">Instagram</a>
+          <a
+            href="http://twitter.com/_blackbottom"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineTwitter title="Twitter" />
+          </a>
+          <a
+            href="https://www.facebook.com/BlackBottomArchives/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineFacebook title="Facebook" />
+          </a>
+          <a
+            href="http://www.instagram.com/blackbottomarchives"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineInstagram title="Instagram" />
+          </a>
         </div>
       </section>
     </footer>
   )
 }
 
-export default Footer;
+export default Footer
