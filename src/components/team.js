83c4a3dff029b2b1e1ee75c97bf6b9d97e8af0d3
Conor Mendenhall <conor.mendenhall@gmail.com>
2020-10-01 00:52:21 -0400

    historical map copy

--- src/components/team.js
+++ src/components/team.js
@@ -7,7 +7,7 @@ import RichText from '../components/rich-text'
 const Team = () => {
   const data = useStaticQuery(graphql`
     query {
-      contentfulArticle(id: { eq: "2c4726e4-1146-5bda-9b01-1cfcb439a7eb" }) {
+      contentfulRichText(id: { eq: "2c4726e4-1146-5bda-9b01-1cfcb439a7eb" }) {
         title
         body {
           json
@@ -18,9 +18,9 @@ const Team = () => {
 
   return (
     <>
-      <h1>{data.contentfulArticle.title}</h1>
-      {data.contentfulArticle?.body?.json && (
-        <RichText data={data.contentfulArticle.body} />
+      <h1>{data.contentfulRichText.title}</h1>
+      {data.contentfulRichText?.body?.json && (
+        <RichText data={data.contentfulRichText.body} />
       )}
     </>
   )
