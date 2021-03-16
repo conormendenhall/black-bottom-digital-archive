925114dd2f6cdfc5ede8c26ad095cb8c442b8728
Conor Mendenhall <conor.mendenhall@gmail.com>
2021-03-15 20:13:33 -0400

    fixed team query

--- src/components/team.js
+++ src/components/team.js
@@ -7,7 +7,7 @@ import RichText from '../components/rich-text'
 const Team = () => {
   const data = useStaticQuery(graphql`
     query {
-      contentfulRichText(id: { eq: "2c4726e4-1146-5bda-9b01-1cfcb439a7eb" }) {
+      contentfulRichText(title: { eq: "Black Bottom Digital Archive Team" }) {
         title
         body {
           json
