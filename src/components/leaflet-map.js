8fc94a60edb154868102a36ba00b4aea843c9295
Conor Mendenhall <conor.mendenhall@gmail.com>
2022-03-07 21:58:59 -0500

    react package upgrades

--- src/components/leaflet-map.js
+++ src/components/leaflet-map.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import Img from 'gatsby-image'
 
 const LeafletMap = ({ center, zoom, className, data }) => {
   return (
-    <Map center={center} zoom={zoom} className={className}>
+    <MapContainer center={center} zoom={zoom} className={className}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank" rel="noopener noreferrer">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank" rel="noopener noreferrer">OpenStreetMap France</a>'
@@ -40,7 +40,7 @@ const LeafletMap = ({ center, zoom, className, data }) => {
             )
           })
         })}
-    </Map>
+    </MapContainer>
   )
 }
 
