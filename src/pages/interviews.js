e08901e0fbb3f95f6b05b21a7f27da91cedae232
Conor Mendenhall <conor.mendenhall@gmail.com>
2022-05-19 22:00:16 -0400

    styling for interview index

--- src/pages/interviews.js
+++ src/pages/interviews.js
@@ -14,26 +14,32 @@ const InterviewsPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Interviews" />
-      <section className="container">
-        <h1>Oral Histories</h1>
-        <p>
-          These oral histories are interviews with some of those who lived in or
-          around Black Bottom in the ‘40s and ‘50s (or had family in the
-          neighborhood). Each entry include transcripts, summaries of interview
-          content, and mp3 streaming audio. If you are interested in doing an
-          oral history interview, please contact us for info.
-        </p>
-        <div id="interviews" className="card-gallery">
-          {interviews}
-        </div>
-      </section>
+      <div className="index-header">
+        <section className="container">
+          <h1>Oral Histories</h1>
+          <p>
+            These oral histories are interviews with some of those who lived in
+            or around Black Bottom in the ‘40s and ‘50s (or had family in the
+            neighborhood). Each entry include transcripts, summaries of
+            interview content, and mp3 streaming audio. If you are interested in
+            doing an oral history interview, please contact us for info.
+          </p>
+        </section>
+      </div>
+      <div className="card-gallery-container">
+        <section className="container">
+          <div id="interviews" className="card-gallery">
+            {interviews}
+          </div>
+        </section>
+      </div>
     </Layout>
   )
 }
 
 export const pageQuery = graphql`
   query {
-    interviews: allContentfulInterview(sort: {order: ASC, fields: title}) {
+    interviews: allContentfulInterview(sort: { order: ASC, fields: title }) {
       edges {
         node {
           id
