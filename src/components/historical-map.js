4bcaf7744350cd3acdaf3ac1d9fcc7f12120c2d1
Conor Mendenhall <conor.mendenhall@gmail.com>
2023-01-05 23:08:16 -0500

    historical sites map

--- src/components/historical-map.js
+++ src/components/historical-map.js
@@ -1,24 +1,14 @@
 import React from 'react'
 
-import { useStaticQuery, graphql } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const HistoricalMap = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      historicalMap: file(
-        relativePath: { eq: "200518_Sanborn Stitch_web_2000px.jpg" }
-      ) {
-        childImageSharp {
-          gatsbyImageData(width: 2000)
-        }
-      }
-    }
-  `)
-
-  const image = getImage(data.historicalMap.childImageSharp.gatsbyImageData)
-
-  return <GatsbyImage image={image} alt="Historical Map of Black Bottom, Detroit" />
+  return (
+    <StaticImage
+      src="../images/200518_Sanborn Stitch_web_2000px.jpg"
+      alt="Historical Map of Black Bottom, Detroit"
+    />
+  )
 }
 
 export default HistoricalMap
--- src/pages/historical-sites.js
+++ src/pages/historical-sites.js
@@ -5,6 +5,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Head from '../components/head'
 import Card from '../components/card'
+import HistoricalMap from '../components/historical-map'
 
 const HistoricalSitesPage = ({ data }) => {
   let sites = data.sites.edges.map((item, key) => (
@@ -18,13 +19,39 @@ const HistoricalSitesPage = ({ data }) => {
         <section className="container">
           <h1>Historical Sites</h1>
           <p>
-            Though Black Bottom and Paradise Valley are long gone, memories and
-            some key sites still remain. These are some of the key locations
-            that existed — some are still around in one form or another, but
-            others were demolished.
+            Black Bottom was the center of Black life in Detroit, beginning in
+            the early 1900's and continuing through the Great Migration, when
+            Black people moved to the city in large numbers from the American
+            South. From the 1950's to the 1970's, the City of Detroit completely
+            demolished the neighborhood in the name of "urban renewal."
+          </p>
+          <p>
+            Hastings Street, which once held the largest concentration of
+            Black-owned institutions and businesses in the city — as well as
+            places that supported its internationally renowned music culture —
+            was demolished to build the Chrysler Freeway.
+          </p>
+          <p>
+            The Black Bottom Historical map shows Black Bottom as it stood in
+            1951, right before the first phase of demolition began.
           </p>
         </section>
       </div>
+      <section className="container">
+        <em>(Click the image to view it in high resolution.)</em>
+        <a href="/200518_Sanborn Stitch_web_8000px.jpg" target="_blank">
+          <HistoricalMap />
+        </a>
+        <em className="map-source">
+          Compiled in 2018 from Sanborn Fire Insurance Maps
+          <br />
+          Sourced from the Library of Congress
+          <br />
+          Sanborn Map Company, Vol. 4, 1922 - Nov 1951
+          <br />
+          Black Bottom Street View / Black Bottom Archives
+        </em>
+      </section>
       <div className="card-gallery-container">
         <section className="container">
           <div id="historicalSites" className="card-gallery">
