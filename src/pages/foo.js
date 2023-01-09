8c5454eec826490de502d9f828007269d9595cbb
Conor Mendenhall <conor.mendenhall@gmail.com>
2023-01-09 11:12:39 -0500

    another try at form

--- src/pages/foo.js
+++ src/pages/foo.js
@@ -4,13 +4,18 @@ import { navigate } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const FooPage = () => {
-  state = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    subject: '',
-    message: '',
+class FooPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      subject: '',
+      message: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange = (event) => {
@@ -36,7 +41,7 @@ const FooPage = () => {
       .catch((error) => alert(error))
   }
 
-  function encode(data) {
+  encode = (data) => {
     return Object.keys(data)
       .map(
         (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
@@ -44,69 +49,85 @@ const FooPage = () => {
       .join('&')
   }
 
-  return (
-    <Layout>
-      <section className="container">
-        <h1>Test Form</h1>
-        <p>*Required</p>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="contact" value="contact" />
-          <p>
-            <label>
-              First Name{' '}
-              <input type="text" name="firstName" onChange={handleChange} />
-            </label>
-            <label>
-              Last Name{' '}
-              <input type="text" name="lastName" onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Email*{' '}
-              <input
-                type="email"
-                name="email"
-                required
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Subject*{' '}
-              <input
-                type="text"
-                name="subject"
-                required
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message*{' '}
-              <textarea
-                name="message"
-                required
-                onChange={handleChange}
-              ></textarea>
-            </label>
-          </p>
-          <div className="button-section">
-            <button type="submit" className="button">
-              Send Message
-            </button>
-          </div>
-        </form>
-      </section>
-    </Layout>
-  )
+  render() {
+    return (
+      <Layout>
+        <section className="container">
+          <h1>Test Form</h1>
+          <p>*Required</p>
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            onSubmit={this.handleSubmit}
+          >
+            <input type="hidden" name="contact" value="contact" />
+            <p>
+              <label>
+                First Name{' '}
+                <input
+                  type="text"
+                  name="firstName"
+                  value={this.state.firstName}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label>
+                Last Name{' '}
+                <input
+                  type="text"
+                  name="lastName"
+                  value={this.state.lastName}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Email*{' '}
+                <input
+                  type="email"
+                  name="email"
+                  value={this.state.email}
+                  required
+                  onChange={this.handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Subject*{' '}
+                <input
+                  type="text"
+                  name="subject"
+                  value={this.state.subject}
+                  required
+                  onChange={this.handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message*{' '}
+                <textarea
+                  name="message"
+                  value={this.state.message}
+                  required
+                  onChange={this.handleChange}
+                ></textarea>
+              </label>
+            </p>
+            <div className="button-section">
+              <button type="submit" className="button">
+                Send Message
+              </button>
+            </div>
+          </form>
+        </section>
+      </Layout>
+    )
+  }
 }
 
-export default FooPage
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<FooPage />)
