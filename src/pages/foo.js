0a7e1c0f7b764b7fb21f597d405ba5a9c7cc1be8
Conor Mendenhall <conor.mendenhall@gmail.com>
2023-01-08 20:54:11 -0500

    form fix

--- src/pages/foo.js
+++ src/pages/foo.js
@@ -2,43 +2,74 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
-const FooPage = () => (
-  <Layout>
-    <section className="container">
-      <h1>Test Form</h1>
-      <p>*Required</p>
-      <form name="contact" data-netlify="true">
-        <p>
-          <label>
-            First Name <input type="text" name="firstName" />
-          </label>
-          <label>
-            Last Name <input type="text" name="lastName" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Email* <input type="email" name="email" required />
-          </label>
-        </p>
-        <p>
-          <label>
-            Subject* <input type="text" name="subject" required />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message* <textarea name="message" required></textarea>
-          </label>
-        </p>
-        <div className="button-section">
-          <button type="submit" className="button">
-            Send Message
-          </button>
-        </div>
-      </form>
-    </section>
-  </Layout>
-)
+const FooPage = () => {
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&')
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': event.target.getAttribute('name'),
+        ...name,
+      }),
+    })
+      .then(() => navigate('/thank-you/'))
+      .catch((error) => alert(error))
+  }
+  
+  return (
+    <Layout>
+      <section className="container">
+        <h1>Test Form</h1>
+        <p>*Required</p>
+        <form
+          name="contact"
+          action="/contact"
+          method="POST"
+          data-netlify="true"
+          onSubmit={handleSubmit}
+        >
+          <input type="hidden" name="contact" value="contact" />
+          <p>
+            <label>
+              First Name <input type="text" name="firstName" />
+            </label>
+            <label>
+              Last Name <input type="text" name="lastName" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Email* <input type="email" name="email" required />
+            </label>
+          </p>
+          <p>
+            <label>
+              Subject* <input type="text" name="subject" required />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message* <textarea name="message" required></textarea>
+            </label>
+          </p>
+          <div className="button-section">
+            <button type="submit" className="button">
+              Send Message
+            </button>
+          </div>
+        </form>
+      </section>
+    </Layout>
+  )
+}
 
 export default FooPage
