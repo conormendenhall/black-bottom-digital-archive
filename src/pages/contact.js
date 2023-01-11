99eaf7379ee8e69c3c43cc42284389f675a53ab7
Conor Mendenhall <conor.mendenhall@gmail.com>
2023-01-10 20:49:32 -0500

    form for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
+import { navigate } from 'gatsby'
+
 import Layout from '../components/layout'
 import Head from '../components/head'
 
-const ContactPage = () => {
-  function encode(data) {
+class ContactPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      subject: '',
+      message: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.encode = this.encode.bind(this)
+  }
+
+  encode = (data) => {
     return Object.keys(data)
       .map(
         (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
@@ -12,75 +28,117 @@ const ContactPage = () => {
       .join('&')
   }
 
-  const handleSubmit = (event) => {
+  handleChange = (event) => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault()
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
+      body: this.encode({
         'form-name': event.target.getAttribute('name'),
-        ...name,
+        ...this.state,
       }),
     })
-      .then(() => navigate('/thank-you/'))
+      .then(() => navigate('/'))
       .catch((error) => alert(error))
   }
 
-  return (
-    <Layout>
-      <Head title="Contact" />
-      <div className="contact-header">
+  render() {
+    return (
+      <Layout>
+        <Head title="Contact" />
+        <div className="contact-header">
+          <section className="container">
+            <h1>Contact</h1>
+            <p>
+              Have any questions, comments, or suggestions about Black Bottom
+              Digital Archive? Please contact us by filling in the form below or
+              sending a message to us at hello@blackbottomarchives.com.
+            </p>
+          </section>
+        </div>
         <section className="container">
-          <h1>Contact</h1>
-          <p>
-            Have any questions, comments, or suggestions about Black Bottom
-            Digital Archive? Please contact us by filling in the form below or
-            sending a message to us at hello@blackbottomarchives.com.
-          </p>
+          <p>*Required</p>
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            onSubmit={this.handleSubmit}
+          >
+            <input type="hidden" name="contact" value="contact" />
+            <p>
+              <label>
+                First Name{' '}
+                <input
+                  type="text"
+                  name="firstName"
+                  value={this.state.firstName}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label>
+                Last Name{' '}
+                <input
+                  type="text"
+                  name="lastName"
+                  value={this.state.lastName}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Email*{' '}
+                <input
+                  type="email"
+                  name="email"
+                  value={this.state.email}
+                  required
+                  onChange={this.handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Subject*{' '}
+                <input
+                  type="text"
+                  name="subject"
+                  value={this.state.subject}
+                  required
+                  onChange={this.handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message*{' '}
+                <textarea
+                  name="message"
+                  value={this.state.message}
+                  required
+                  onChange={this.handleChange}
+                ></textarea>
+              </label>
+            </p>
+            <div className="button-section">
+              <button type="submit" className="button">
+                Send Message
+              </button>
+            </div>
+          </form>
         </section>
-      </div>
-      <section className="container">
-        <p>*Required</p>
-        <form
-          name="contact"
-          action="/contact"
-          method="POST"
-          data-netlify="true"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="contact" value="contact" />
-          <p>
-            <label>
-              First Name <input type="text" name="firstName" />
-            </label>
-            <label>
-              Last Name <input type="text" name="lastName" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Email* <input type="email" name="email" required />
-            </label>
-          </p>
-          <p>
-            <label>
-              Subject* <input type="text" name="subject" required />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message* <textarea name="message" required></textarea>
-            </label>
-          </p>
-          <div className="button-section">
-            <button type="submit" className="button">
-              Send Message
-            </button>
-          </div>
-        </form>
-      </section>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
 
 export default ContactPage
