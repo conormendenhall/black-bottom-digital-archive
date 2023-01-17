ca50c33b21641968d8fee0c9eb035aa850bf238f
Conor Mendenhall <conor.mendenhall@gmail.com>
2023-01-16 22:36:57 -0500

    remove proptypes from layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import Header from './header'
 import Footer from './footer'
@@ -20,10 +12,6 @@ const Layout = ({ children }) => {
       <Footer />
     </div>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
